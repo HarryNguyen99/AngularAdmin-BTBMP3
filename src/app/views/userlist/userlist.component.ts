36f72a1dc1de97ff3fdf2747db074a6819273a3e
CaoXuanQuy1985 <cun.kin1985.working@gmail.com>
2020-07-30 13:56:53 +0700

    refactoring code

--- src/app/views/userlist/userlist.component.ts
+++ src/app/views/userlist/userlist.component.ts
@@ -15,7 +15,6 @@ export class UserlistComponent implements OnInit {
   userList: Observable<User[]>;
 
   ngOnInit() {
-    this.getInformationServer();
     this.reloadData();
   }
 
@@ -44,53 +43,5 @@ export class UserlistComponent implements OnInit {
     );
   }
 
-  title = 'myHighchart';
-
-  data: Statistic[] = [];
-
-  getInformationServer(): any {
-    this.adminService.getInformationStatistic().subscribe(
-      result => {
-        for (let i = 0; i < result.length; i++) {
-          for (let j = 0; j < result[i].length; j++) {
-            if (j == 0) {
-              this.data[i] = new Statistic();
-              let formatDateStr = result[i][0].substr(0, 10);
-              this.data[i].setName(formatDateStr);
-            } else {
-              this.data[i].data = [];
-              this.data[i].data[0] = result[i][1];
-            }
-          }
-        }
-      }
-    );
-  }
-
-  highcharts = null;
-  chartOptions = {};
-
-  enableChart() {
-    console.log(this.data);
-
-    this.highcharts = Highcharts;
-    this.chartOptions = {
-      chart: {
-        type: "column"
-      },
-      title: {
-        text: "Biểu Đồ Theo Dõi Số Lượng Người Đăng Kí Mới Theo Ngày"
-      },
-      xAxis:{
-        categories:["Đơn Vị Theo Ngày"]
-      },
-      yAxis: {
-        title:{
-          text:"Số Lượng Người"
-        }
-      },
-      series: this.data
-    };
-  }
 }
 
